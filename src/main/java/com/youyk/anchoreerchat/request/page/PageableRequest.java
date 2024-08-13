ac14472b167cb1947ff48717ab296f1702ccc8c2
YoungKyonYou <man.spride1324@gmail.com>
2024-08-13 16:45:17 +0900

    feat: 과거 채팅 메시지를 요청하는 클래스 추가

---
+++ src/main/java/com/youyk/anchoreerchat/request/page/PageableRequest.java
@@ -0,0 +1,19 @@
+package com.youyk.anchoreerchat.request.page;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+
+import org.springframework.data.domain.PageRequest;
+
+@Builder
+public record PageableRequest(
+        @NotNull
+        int page,
+        @NotNull
+        int size
+) {
+    public PageRequest fromPageRequest() {
+        return PageRequest.of(this.page, this.size);
+    }
+}
+
