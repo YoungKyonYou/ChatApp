89383fcb79e57a4faae57421e7d036935f0f4504
YoungKyonYou <man.spride1324@gmail.com>
2024-08-13 16:31:03 +0900

    feat: 채팅 엔티티 생성

---
+++ src/main/java/com/youyk/anchoreerchat/entity/chat/ChatRoomMember.java
@@ -0,0 +1,38 @@
+package com.youyk.anchoreerchat.entity.chat;
+
+import com.youyk.anchoreerchat.entity.message.Message;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class ChatRoomMember {
+    @GeneratedValue
+    @Id
+    private Long chatRoomMemberId;
+
+    private Long senderId;
+
+    private Long chatRoomId;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "message_id")
+    private Message message;
+
+    private LocalDateTime createdAt;
+
+    public static ChatRoomMember of(final Long memberId, final Long chatRoomId, final Message message) {
+        return ChatRoomMember.builder()
+                .senderId(memberId)
+                .chatRoomId(chatRoomId)
+                .message(message)
+                .createdAt(LocalDateTime.now())
+                .build();
+    }
+}
